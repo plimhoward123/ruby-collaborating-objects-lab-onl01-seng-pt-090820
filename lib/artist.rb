ac2a37ef3bd3f0aef4f443e830fb3aebb702ff98
Plimon Howard <plimon.howard@gmail.com>
2020-10-14 17:22:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,19 +6,41 @@ class Artist
     def initialize(name)
       @name = name
       @songs = []
+<<<<<<< HEAD
       self.save
+=======
+      @@all << self
+>>>>>>> d01b1c8c5ce2598736ec079564e771ee258d120a
     end
 
     def add_song(song)
       self.songs << song
     end
 
+<<<<<<< HEAD
     def self.find_or_create_by_name(val)
       tmp = self.all.find {|value| value.name == val}
       if tmp
           return tmp
       else
         newArt = Artist.new(val)
+=======
+    def save
+      @@all << self
+    end
+
+    def self.all
+
+    end
+
+    def self.find_or_create_by_name(val)
+      tmp = self.all.find {|value| value.name == val}
+      if tmp
+        tmp
+      else
+        newArt = Artist.new(val)
+        newArt.save
+>>>>>>> d01b1c8c5ce2598736ec079564e771ee258d120a
         newArt
       end
     end
@@ -26,6 +48,7 @@ class Artist
     def print_songs
       self.songs.each { |value|  puts value.name}
     end
+<<<<<<< HEAD
 
     def save
       @@all << self
@@ -34,4 +57,6 @@ class Artist
     def self.all
       @@all
     end
+=======
+>>>>>>> d01b1c8c5ce2598736ec079564e771ee258d120a
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+>>>>>>> d01b1c8c5ce2598736ec079564e771ee258d120a
 class Song
   attr_accessor :artist, :name
   @@all = []
 
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     self.save
+=======
+>>>>>>> d01b1c8c5ce2598736ec079564e771ee258d120a
   end
 
   def self.new_by_filename(value)
@@ -21,8 +27,12 @@ class Song
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
+<<<<<<< HEAD
     self.artist.add_song(self)
     #binding.pry
+=======
+    artist.add_song(self)
+>>>>>>> d01b1c8c5ce2598736ec079564e771ee258d120a
   end
 
   def save
